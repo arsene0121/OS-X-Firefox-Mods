d2dfe2616eddf427cdfbfae4814dc432a2b51f42
tburgin <tom.burgin@mac.com>
2014-02-27 14:35:35 -0500

    Update FirefoxAuth-Kerberos.sh

--- FirefoxAuth-Kerberos.sh
+++ FirefoxAuth-Kerberos.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #Written by Tom Burgin | NIMH | burgintj@nimh.nih.gov
 #run with sudo
-#This script will enable kerberos pass through to all nih.gov websites in firefox.
+#This script will enable kerberos pass through to all domain.gov websites in firefox.
 
 globalSettings(){
 
-domain="nih.gov" ## Set your Kerberos Domain
 
 if grep 'network.negotiate-auth.trusted-uris' /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js;
 then
 	sudo sed '/network.negotiate-auth.trusted-uris/d' /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js > /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js.e;
-	echo 'pref("network.negotiate-auth.trusted-uris", "$domain");' >> /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js.e;
+	echo 'pref("network.negotiate-auth.trusted-uris", "domain.gov");' >> /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js.e;
 	mv /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js.e /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js
-	echo "GLOBAL: network.negotiate-auth.trusted-uris is now set to $domain using SED"
+	echo "GLOBAL: network.negotiate-auth.trusted-uris is now set to domain.gov using SED"
 else
-    echo 'pref("network.negotiate-auth.trusted-uris", "$domain");' >> /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js;
-    	echo "GLOBAL: network.negotiate-auth.trusted-uris is now set to $domain"
+    echo 'pref("network.negotiate-auth.trusted-uris", "domain.gov");' >> /Applications/Firefox.app/Contents/MacOS/defaults/pref/channel-prefs.js;
+    	echo "GLOBAL: network.negotiate-auth.trusted-uris is now set to domain.gov"
 
 fi
 
@@ -40,12 +39,12 @@ do
 					if grep 'network.negotiate-auth.trusted-uris' /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js;
 					then
 						sudo sed '/network.negotiate-auth.trusted-uris/d' /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js > /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js.e;
-						echo 'user_pref("network.negotiate-auth.trusted-uris", "$domain");' >> /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js.e;
+						echo 'user_pref("network.negotiate-auth.trusted-uris", "domain.gov");' >> /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js.e;
 						mv /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js.e /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js;
-						echo "USER $i: network.negotiate-auth.trusted-uris is now set to $domain using SED";
+						echo "USER $i: network.negotiate-auth.trusted-uris is now set to domain.gov using SED";
 					else
-    					echo 'user_pref("network.negotiate-auth.trusted-uris", "$domain");' >> /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js.;
-    					echo "USER $i: network.negotiate-auth.trusted-uris is now set to $domain";
+    					echo 'user_pref("network.negotiate-auth.trusted-uris", "domain.gov");' >> /Users/$i/Library/Application\ Support/Firefox/Profiles/$p/prefs.js.;
+    					echo "USER $i: network.negotiate-auth.trusted-uris is now set to domain.gov";
     				fi	
     			done
 	fi
